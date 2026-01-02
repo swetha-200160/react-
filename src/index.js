6a4d296c3cea488da2fd276d35cb5a9660a77cd1
Swetha Suresh <swethasuresh@canvendor.com>
2026-01-02 13:06:23 +0530

    adding jenkins file

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <HashRouter>
     <App />
   </HashRouter>,
-)
-
+);
