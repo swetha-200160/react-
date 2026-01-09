b36aac8d095443205e20d16582d3de1e7f09b808
Swetha Suresh <swethasuresh@canvendor.com>
2026-01-09 18:11:02 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Box } from '@mui/material';
-
-import './App.css';
-import ExerciseDetail from './pages/ExerciseDetail';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
-const App = () => (
-  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/exercise/:id" element={<ExerciseDetail />} />
-    </Routes>
-    <Footer />
-  </Box>
-);
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React App Running in Docker 🚀</h1>
+      <p>Docker Compose + Volume + Network</p>
+    </div>
+  );
+}
 
 export default App;
